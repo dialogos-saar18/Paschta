a45d78596a354800f23a99f46ffaf35c1cacaeb0
TheresaSchmidt <36304889+TheresaSchmidt@users.noreply.github.com>
2019-02-03 16:24:23 +0100

    self.recipe ist jetzt ein Rezeptobjekt, keine Liste mehr

--- paschtaClient.py
+++ paschtaClient.py
@@ -14,25 +14,28 @@ from java.awt import Color
 from java.awt import Dimension
 from java.lang import Class
 
-
+# Hilfsfunktion; ruft die Methoden auf, die die Informationen auslesen
 def call(category, term, recipe):
-    #ruft die Methoden auf, die die Informationen auslesen
-    if category == "anleitung": # gibt die komplette Anleitung oder einen Schritt zurück
-        return(recipe.get_schritt(term)) # term Element von {"first","next","repeat","all","previous", "last"}
-    elif category == "zutaten": # gibt alle Zutaten oder einzelne Zutaten zurück
-        return(recipe.get_zutat(term)) 
-    elif category == "eigenschaft":# auslesen der Eigenschaften wie Arbeitszeit, Schwierigkeitsgrad etc.
-        print(recipe.get_property(term))
+    # gibt die komplette Anleitung oder einen Schritt zurück
+    if category == "anleitung":
+        # term Element von {"first","next","repeat","all","previous", "last"}
+        return(recipe.get_schritt(term))
+    # gibt alle Zutaten oder einzelne Zutaten zurück
+    elif category == "zutaten": 
+        return(recipe.get_zutat(term))
+    # auslesen der Eigenschaften wie Arbeitszeit, Schwierigkeitsgrad etc.
+    elif category == "eigenschaft":
         return(recipe.get_property(term))
-    else:#works
+    else:
         print("Fehler in call; Kategorie " + category + " ist ungültig")
         return ("ungültig")
-        ##raise Error! 
+        # TODO: raise Error! 
 
-
+# TODO: Leerzeichen
 
 class Main(Client):
     def __init__(self):
+        self.recipe = None
         pass    
 
     def stateChanged(self, cs):
@@ -44,8 +47,6 @@ class Main(Client):
     def reset(self):
         print "reset"
 
-    #value has type string list (from the Value class)
-    #die rezept-referenz ist ein int-Value
     # "URL"
     # "ingredients"
     # "ingredients" rezept
@@ -59,119 +60,117 @@ class Main(Client):
     # "exists_zutat" suchbegriff
     def output(self, value):
         value = list(value)
+        # value hat jetzt den Typ Value list
+        # TODO: key = value[0].getString().strip('"')
 
-        #Quelltext für Rezept abfragen
+        # Quelltext für Rezept abfragen
         if value[0].getString().strip('"') == "URL":
             self.gui()
-            #recipe = Recipe('https://www.chefkoch.de/rezepte/785281181805506/Spinat-Cannelloni-al-Forno.html')
-            #self.recipes=[recipe]
-            print("created recipe")            
+            print("created recipe")
+            # TODO: print title
 
-        #erstellt Liste mit allen Zutaten 
+        # erstellt Liste mit allen Zutaten 
         elif value[0].getString().strip('"')=="ingredients":
-            if len(value)==1:
-                self.send(self.recipes[0].ingredients())
-            else:
-                self.send(self.recipes[int(value[-1].getString())].ingredients())
-        #erstellt Liste mit allen Einheiten
-        elif value[0].getString().strip('"')=="einheiten":
-            if len(value)==1:
-                self.send(self.recipes[0].einheiten())
-            else:
-                self.send(self.recipes[int(value[-1].getString())].einheiten())
-        #fragt den Titel des Rezepts ab
-        elif value[0].getString().strip('"')=="titel":
-            if len(value)==1:
-                self.send(self.recipes[0].get_title())
-            else:
-                self.send(self.recipes[int(value[-1].getString())].get_title())
-
-        #kompletten Einkaufszettel schreiben
-        elif value[0].getString().strip('"')=="einkaufszettel":
-            if len(value)==1:
-                self.send(self.recipes[0].einkaufszettel("all"))
-            else:
-                self.send(self.recipes[int(value[-1].getString())].einkaufszettel())
-
-        #einzelne Zutat auf Einkaufszettel
-        elif value[0].getString().strip('"')=="Zettel":
-            self.recipes[0].einkaufszettel(value[1].getString().strip('"'))
-
-        #gibt Antwort zurück, ob eine Zutat gebraucht wird
-        elif value[0].getString().strip('"')=="exists_zutat":
-            #if len(value)==1:
+            self.send(self.recipe.ingredients())
             
-            self.send(self.recipes[0].contains(value[1].getString().strip('"')))
-            #else:
-             #   self.send(self.recipes[int(str(value[-1]))].einkaufszettel())
+        # erstellt Liste mit allen Einheiten
+        elif value[0].getString().strip('"')=="einheiten":
+            self.send(self.recipe.einheiten())
+                
+        # fragt den Titel des Rezepts ab
+        elif value[0].getString().strip('"')=="titel":
+            self.send(self.recipe.get_title())
 
-        #
+        # kompletten Einkaufszettel schreiben
+        elif value[0].getString().strip('"')=="einkaufszettel":
+            self.send(self.recipe.einkaufszettel("all"))
+            # TODO: what does this send?
+
+        # einzelne Zutat auf Einkaufszettel (keine Rückmeldung an den Dialog)
+        elif value[0].getString().strip('"')=="Zettel":
+            self.recipe.einkaufszettel(value[1].getString().strip('"'))
+
+        # gibt Antwort zurück, ob eine Zutat gebraucht wird
+        elif value[0].getString().strip('"')=="exists_zutat":
+            self.send(self.recipe.contains(value[1].getString().strip('"')))
+
+        # TODO
         elif value[0].getString().strip('"')=="zutaten":
-            self.send(self.recipes[0].get_zutat(value[1].getString().strip('"')))
-        #gibt zurück, für wie viele Personen das Rezept gedacht ist
+            self.send(self.recipe.get_zutat(value[1].getString().strip('"')))
+            
+        # gibt zurück, für wie viele Personen das Rezept kalkuliert ist
         elif value[0].getString().strip('"')=="portionen":
-            #if len(value)==1:
             if value[1].getString().strip('"')== "wert":
-                self.send(str(self.recipes[0].get_portions()))
-            #else:
-                #self.send(self.recipes[int(str(value[-1]))].einkaufszettel())
+                self.send(str(self.recipe.get_portions()))
+            # TODO: else
 
-        #fürs umrechnen nach Personen
+        # fürs Umrechnen nach Personen
         elif value[0].getString().strip('"') == "Personen":
-            self.send(self.recipes[0].umrechnen(value[1].getString().strip('"'),u'Personen'))
-        #fürs umrechnen nach Zutaten
+            self.send(self.recipe.umrechnen(value[1].getString().strip('"'),u'Personen'))
+        # fürs Umrechnen nach Zutaten
         elif value[0].getString().strip('"')=="Zutaten":
             z = value[1].getString().strip('"')
             m = value[2].getString().strip('"')
             e = value[3].getString().strip('"')
-            self.send(self.recipes[0].umrechnen([m,e],z))
+            self.send(self.recipe.umrechnen([m,e],z))
 
+        # reset Schrittindex (wenn <TODO>)
         elif value[0].getString().strip('"') == "ende":
-            self.recipes[0].schritt = 0
+            self.recipe.schritt = 0
 
-        elif len(value)==3:
-            self.send(call(value[0].getString().strip('"'), value[1].getString().strip('"'), self.recipes[int(str(value[-1]))]))
+        # TODO: what does it do and what it the first element? (make this an elif and throw error if not match
         else:
-            self.send(call(value[0].getString().strip('"'), value[1].getString().strip('"'), self.recipes[0]))
-            
-        print "output: " + "done"
+            self.send(call(value[0].getString().strip('"'), value[1].getString().strip('"'), self.recipe))
+
+        # TODO: test
+        print "output: " + "done (" + str(value) + ")"
 
     def getName(self):
-        return "Jython demo client"
+        return "Paschta Client"
 
     def error(self, throwable):
         print "error"
 
+    # öffnet eine GUI, in der man eine URL eintippen kann
+    # erstellt ein Rezeptobjekt anhand der URL
+    def gui(self):
 
-    def gui(self):#returns URL in unicode
+        # Hilfsfunktion für event  
+        # erstellt ein Rezeptobjekt anhand einer URL
+        # schließt die GUI
+        def create(event):
+            url = field.getText()
+            self.recipe = Recipe(url)
+            frame.dispose()
+            # der Dialog wartet, bis "continue" gesendet wird
+            self.send("continue")
+
+        # Frame erstellen
         frame = JFrame('URL eingeben',
                 defaultCloseOperation = JFrame.EXIT_ON_CLOSE,
                 size = (480,200),
                        )
         frame.setLayout(None)
         
-        def testact(event):
-            frame.setVisible(False)
-            url = field.getText()
-            recipe = Recipe(url)
-            self.recipes=[recipe]
-            self.send("continue")
-
-         
+        # Text im Frame
         fieldlabel = JLabel()
         fieldlabel.setText("<html><font size=+1>Geben Sie die Internetadresse des Rezepts ein</font></html>")
         fieldlabel.setBounds(20,20,500,40)
         frame.add(fieldlabel)
 
+        # Textfeld im Frame
         field = JTextField()
         field.setText("https://www.chefkoch.de/rezepte/...")
         field.setBounds(20,60,411,40)
         frame.add(field)
-        
-        button = JButton("Los!",actionPerformed=testact)
+
+        # Button im Frame
+        # ruft Hilfsfunktion create auf
+        button = JButton("Los!", actionPerformed = create)
         button.setBounds(155,100,150,30)
         frame.add(button)
-        
+
+        #Frame anzeigen
         frame.setVisible(True)
 
 
